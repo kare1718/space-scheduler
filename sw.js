04d4cfc35a0d3b5e6fc1d7c425d9033f26faff42
kare1718 <kare1718@users.noreply.github.com>
2026-03-21 02:02:39 +0900

    Space Scheduler - 초기 배포

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='spaceschV2';
+const URLS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    if(res.status===200){const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));}
+    return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
