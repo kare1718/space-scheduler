4b1eacb9a992d8869c4c62aded99f7ce9f1f78f1
kare1718 <kare1718@users.noreply.github.com>
2026-03-21 02:12:12 +0900

    기기 간 공유: 내보내기/가져오기 기능 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spaceschV2';
+const CACHE='spaceschV3';
 const URLS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
